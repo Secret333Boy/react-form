dd1cc31a2d1692357296b1a07f509a0e5b5090f2
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-10-09 03:01:40 +0300

    Typo fixed

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const emailVaidation = new RegExp(
+const emailValidation = new RegExp(
   // eslint-disable-next-line no-control-regex
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 );
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
     return;
   }
 
-  if (!email.match(emailVaidation)) {
+  if (!email.match(emailValidation)) {
     res.status(400);
     res.send('Email validation fail');
     return;
