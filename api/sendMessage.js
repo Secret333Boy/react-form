d31193e2fc8ea912a63f69b6949165406d061545
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-10-09 04:16:38 +0300

    Try catch block fix & rateLimit partly implemented

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -5,27 +5,42 @@ const emailValidation = new RegExp(
   // eslint-disable-next-line no-control-regex
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 );
+
+const rateLimit = 10;
+const rateTimeReset = 10;
+const rateList = {};
+
 module.exports = async (req, res) => {
-  const fName = req.query.fName;
-  const sName = req.query.sName;
-  const email = req.query.email;
-  const message = sanitizeHtml(req.query.message, {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-  });
-
-  if (!email || !fName) {
-    res.status(400);
-    res.send('Bad request');
-    return;
-  }
-
-  if (!email.match(emailValidation)) {
-    res.status(400);
-    res.send('Email validation fail');
-    return;
-  }
-
   try {
+    const origin = req.headers.origin;
+    if (!rateList[origin]) rateList[origin] = 0;
+
+    rateList[origin]++;
+    if (rateList[origin] > rateLimit) {
+      res.status(429);
+      res.send('Too many requests');
+      return;
+    }
+
+    const fName = req.query.fName;
+    const sName = req.query.sName;
+    const email = req.query.email;
+    const message = sanitizeHtml(req.query.message, {
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+    });
+
+    if (!email || !fName) {
+      res.status(400);
+      res.send('Bad request');
+      return;
+    }
+
+    if (!email.match(emailValidation)) {
+      res.status(400);
+      res.send('Email validation fail');
+      return;
+    }
+
     const transporter = await nodemailer.createTransport({
       service: 'GMAIL',
       auth: {
